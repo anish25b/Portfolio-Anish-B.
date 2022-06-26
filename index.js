891c1f9dc6e074754da20d1e7db8cc9cf794bf1d
anish25b <70747019+anish25b@users.noreply.github.com>
2022-06-26 15:25:14 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-
- }
-
- const switchBtn = document.querySelector(".switch-btn");
- switchBtn.addEventListener("click", myFunction);
